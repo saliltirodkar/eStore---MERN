a2ede2799efa376dd374dd8500977564ba126601
unknown <salilt.001@gmail.com>
2023-09-29 17:34:48 +0530

    First commit

---
+++ src/Components/CatNav/CatNav.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./_cat-nav.scss";
+
+const CatNav = () => {
+  return (
+    <div>
+      <div className="cat-nav-container container">
+        <ul>
+          <li className="list-items">
+            <a href="#"> Men</a>
+          </li>
+          <li className="list-items">
+            <a href="#"> Women</a>
+          </li>
+          <li className="list-items">
+            <a href="#"> Kids</a>
+          </li>
+          <li className="list-items">
+            <a href="#"> Best offers</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CatNav;
